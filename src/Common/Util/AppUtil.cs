7edc0c11a9c4d6d8f690b3e7563427807cc94aeb
JerryBian <JerryBian@outlook.com>
2025-12-26 11:43:21 +0800

    7.0

--- src/Common/Util/AppUtil.cs
+++ src/Common/Util/AppUtil.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace Pika.Common.Util;
 
@@ -9,71 +8,20 @@ public static class AppUtil
     {
         get
         {
-            // Try multiple assemblies because single-file publish or native host can change EntryAssembly behavior
-            var assembliesToCheck = new Assembly[]
-            {
-                Assembly.GetEntryAssembly(),
-                Assembly.GetExecutingAssembly(),
-                Assembly.GetCallingAssembly(),
-                typeof(AppUtil).Assembly
-            };
-
-            foreach (var asm in assembliesToCheck)
-            {
-                var v = GetVersionFromAssembly(asm);
-                if (!string.IsNullOrEmpty(v)) return v;
-            }
-
-            // As a last resort return 1.0
-            return "1.0";
-        }
-    }
-
-    private static string GetVersionFromAssembly(Assembly asm)
-    {
-        if (asm == null) return null;
-
-        // 1) AssemblyInformationalVersionAttribute
-        try
-        {
+            var asm = typeof(AppUtil).Assembly;
             var infoAttr = asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
             if (!string.IsNullOrEmpty(infoAttr?.InformationalVersion))
             {
                 return infoAttr.InformationalVersion;
             }
-        }
-        catch { }
 
-        // 2) File/Product version via file location (may be unavailable for single-file apps)
-        try
-        {
-            var location = asm.Location;
-            if (!string.IsNullOrEmpty(location))
-            {
-                var fileVer = FileVersionInfo.GetVersionInfo(location)?.ProductVersion;
-                if (!string.IsNullOrEmpty(fileVer))
-                {
-                    return fileVer;
-                }
-            }
-        }
-        catch { }
-
-        // 3) AssemblyName version (Major.Minor)
-        try
-        {
             var version = asm.GetName().Version;
-            if (version != null)
+            if (version != null && (version.Major != 0 || version.Minor != 0))
             {
-                // If version has non-zero parts return as Major.Minor or Major.Minor.Build
-                if (version.Major != 0 || version.Minor != 0 || version.Build != 0)
-                {
-                    return version.ToString(2);
-                }
+                return $"{version.Major}.{version.Minor}";
             }
-        }
-        catch { }
 
-        return null;
+            return "1.0";
+        }
     }
 }
